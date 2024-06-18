220a129b634cb2314f4624741f010e39c35b01bd
João Antônio A. D. S. S. <joao.antonio.amorim.8@gmail.com>
2024-06-17 21:17:10 -0300

    Adição
    - Adicionado um pouco de estética para o primeiro "quadro".
    
    - Adicionado lógica no primeiro quadro.

--- JS-HTML-CSS-Projeto02/logica.js
+++ JS-HTML-CSS-Projeto02/logica.js
@@ -1,3 +1,15 @@
-function clickmulti() {
-    window.alert('tudo certo')
-}
+    
+    
+    
+    function clickmulti() {
+        
+        let pnumber = document.querySelector('input#pnumber')
+        let snumber = document.querySelector('input#snumber')
+        let presp = document.querySelector('p#pmulti')
+        
+        if (pnumber.value == 0 || snumber.value == 0) {
+            
+            window.alert ('ERRO! Favor inserir dados!')
+            
+        }
+    }
