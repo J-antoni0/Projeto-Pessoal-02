f831db212bddaab0f4bbb366d4b992e2cd64815c
João Antônio A. D. S. S. <joao.antonio.amorim.8@gmail.com>
2024-06-21 21:22:07 -0300

    Alterações na lógica
    - Alterado lógica.

--- JS-HTML-CSS-Projeto02/logica.js
+++ JS-HTML-CSS-Projeto02/logica.js
@@ -1,57 +1,101 @@
-    
-    
+    function clicou() {
+        let presp = document.querySelector('p#pmulti')
+        let pnumber = document.querySelector('input#pnumber')
+        let snumber = document.querySelector('input#snumber')
+        pnumber.value = ' '
+        snumber.value = ' '
+        presp.innerHTML = (' ')
+
+    }
     
     function clickmulti() {
         
         let pnumber = document.querySelector('input#pnumber')
         let snumber = document.querySelector('input#snumber')
         let presp = document.querySelector('p#pmulti')
-        let opcao = document.createElement('option')
-        let escolha = document.querySelector('select#esc')
+        let escolha = document.querySelector('select#esc').value
 
-        switch (escolha.opcao) {
+        if (pnumber.value == 0 || snumber.value == 0) {
             
-            case opcao[0]:
-                if (pnumber.value == 0 || snumber.value == 0) {
-            
-                    window.alert ('ERRO! Favor inserir dados!')
-            
-                } else {
-            
-                    let reversao1 = Number(pnumber.value)
-                    let reversao2 = Number(snumber.value)
-                    let resultmulti = reversao1 + reversao2
-                    presp.innerHTML = (`Resultado = ${resultmulti}`)
+            window.alert ('ERRO! Favor inserir dados!')
+    
+        }
 
 
+
+        switch (escolha) {
+            
+            case 'Somar':
+                if (pnumber.value > 0 && snumber.value > 0) {
+                let reversao1 = Number(pnumber.value)
+                let reversao2 = Number(snumber.value)
+                let resultmulti = reversao1 + reversao2
+                presp.innerHTML = (`Resultado = ${resultmulti}`)
                 }
-        
                 pnumber.focus()
                 break
-            
-            case opcao[1]:
-                if (pnumber.value == 0 || snumber.value == 0) {
-            
-                    window.alert ('ERRO! Favor inserir dados!')
-            
-                } else {
+                    
+            case 'Subtrair':
+                if (pnumber.value > 0 && snumber.value > 0) {
         
                     let reversao1 = Number(pnumber.value)
                     let reversao2 = Number(snumber.value)
                     let resultmulti = reversao1 - reversao2
-                    presp.innerHTML = (`Resultado = ${resultmulti}`)
-
+                    presp.innerHTML = (`Resultado = ${resultmulti}`)                
 
                 }
         
                 pnumber.focus()
                 break
+
+            case 'Multiplicar':
+                if (pnumber.value > 0 && snumber.value > 0) {
+            
+                    let reversao1 = Number(pnumber.value)
+                    let reversao2 = Number(snumber.value)
+                    let resultmulti = reversao1 * reversao2
+                    presp.innerHTML = (`Resultado = ${resultmulti}`)
+
+
+                }
+                pnumber.focus()
+                break
+            
+                case 'Divisao Real':
+                    if (pnumber.value > 0 && snumber.value > 0) {
+                
+                        let reversao1 = Number(pnumber.value)
+                        let reversao2 = Number(snumber.value)
+                        let resultmulti = reversao1 / reversao2
+                        presp.innerHTML = (`Resultado = ${resultmulti}`)
+    
+    
+                    }
+                    pnumber.focus()
+                    break 
+
+                case 'Divisao Inteira':
+                    if (pnumber.value > 0 && snumber.value > 0) {
+                
+                        let reversao1 = Number(pnumber.value)
+                        let reversao2 = Number(snumber.value)
+                        let resultmulti = reversao1 % reversao2
+                        presp.innerHTML = (`Resultado = ${resultmulti}`)
+    
+    
+                    }
+                    pnumber.focus()
+                    break
+                
         }
     }
 
     function quadroum() {
         
         let presp = document.querySelector('p#pmulti')
+        let pnumber = document.querySelector('input#pnumber')
+        let snumber = document.querySelector('input#snumber')
+        pnumber.value = ' '
+        snumber.value = ' '
         presp.innerHTML = (' ')
-
     }
